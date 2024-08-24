db1820469fb4f4595cc8b20c5b93103c5d1e3ad8
kazvee <kazveedev@gmail.com>
2024-08-23 20:19:15 -0600

    Add greet_user.py

---
+++ greet_user.py
@@ -0,0 +1,7 @@
+def greet_user():
+  print(
+      "Welcome to Snake Bytes Dinner Planner!\n\n"
+      "You will be asked to provide individual dinner meal ideas, one at a time.\n"
+      "A personalized dinner plan for the upcoming week will be generated for you.\n"
+      "Plus, I'll include a little surprise!\n\n"
+      "Let's get started!\n")
